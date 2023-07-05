314c555b354a15da061baec2d1824dfd6cc09c20
oc-dave <135023771+oc-dave@users.noreply.github.com>
2023-07-05 11:41:26 +0100

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,14 @@
+package Example1;
+
+public class Example {
+    public static void main(String[] args) {
+        boolean condition = true;
+        int counter = 1;
+        while(counter < 10){
+            System.out.println(counter);
+            counter++;
+
+        }
+
+    }
+}
