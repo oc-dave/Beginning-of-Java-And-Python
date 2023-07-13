434c30727baea977151626c1afc2fc29f5977d6a
oc_dave <davidokechukwu18@gmail.com>
2023-07-13 16:49:31 +0100

    ...

--- list.py
+++ list.py
@@ -14,22 +14,23 @@
 #
 # print(total)
 #
-# # numbers = [10, 20, 30, 40, 50]
-# # total = 0
-# #
-# # for x in numbers[1::2]:
-# #     total += x
-# #
-# # print(total)
+# numbers = [10, 20, 30, 40, 50]
+# total = 0
+#
+# for x in numbers[1::2]:
+#
+#     total += x
+#
+# print(total)
 
-numbers = [10, 20, 30, 40, 50]
-squares = []
-total = 0
-
-for x in numbers:
-    square = x ** 2
-    squares.append(square)
-    total += square
-
-print("Squared numbers:", squares)
-print("Sum of squared numbers:", total)
+# numbers = [10, 20, 30, 40, 50]
+# squares = []
+# total = 0
+#
+# for x in numbers:
+#     square = x ** 2
+#     squares.append(square)
+#     total += square
+#
+# print("Squared numbers:", squares)
+# print("Sum of squared numbers:", total)
