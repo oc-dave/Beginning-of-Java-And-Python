850a74cf3c80fe0a9504df385c2aedc7720fffc5
oc_dave <davidokechukwu18@gmail.com>
2023-07-18 11:09:22 +0100

    ...

---
+++ dave_package/classwork.py
@@ -0,0 +1,13 @@
+import math
+
+num = int(input("Enter a number: "))
+
+
+def divide_or_square(num):
+    if num % 5 == 0:
+        return math.sqrt(num)
+    else:
+        return num % 5
+
+
+print(divide_or_square(num))67
