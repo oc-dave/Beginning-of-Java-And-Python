8b867c32ca4e22a585e20ac708dc42deff02826e
oc_dave <davidokechukwu18@gmail.com>
2023-08-15 14:52:00 +0100

    ...

--- dave_package/set_list.py
+++ dave_package/set_list.py
@@ -1,10 +1,18 @@
-list1 = [20,30, 30, 60, 65, 75, 80]
+# list1 = [20,30, 30, 60, 65, 75, 80]
+# list2 = [42, 30, 80, 65, 68, 88, 95]
+#
+# intersection = []
+# for element in list1:
+#     if element in list2:
+#         intersection.append(element)
+#
+# intersect = tuple(intersection)
+# print(intersect)
+
+list1 = [20, 30, 60, 65, 65, 75, 80]
 list2 = [42, 30, 80, 65, 68, 88, 95]
 
-intersection = []
-for element in list1:
-    if element in list2:
-        intersection.append(element)
-
-intersect = tuple(intersection)
-print(intersect)
+new_list = {element for element in list1 if element in list2}
+new_list = tuple(new_list)
+var = new_list[::-1]
+print(var)
